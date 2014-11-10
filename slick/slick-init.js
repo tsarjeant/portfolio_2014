66ec6b8181679c0a5485bc90610d17403e6fa22b
tsarjeant <tsarjeant503@gmail.com>
2014-11-10 00:59:08 -0600

    adding slick files, updating section headers

---
+++ slick/slick-init.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+$('.tt-scroller').slick({
+    infinite: true,
+    arrows:true,
+    cssEase:'ease',
+    dots: true,
+    slide: 'div',
+    swipe: true,
+  });
+});
